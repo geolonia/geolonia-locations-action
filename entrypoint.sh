d776983bb3e7892cbcb7141ae1372cbd4e927eb4
kamataryo <kamataryo@users.noreply.github.com>
2022-07-04 10:58:44 +0900

    Test commit

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,11 +9,11 @@ OUTPUT_DIR=$2
 # $1: input files (shape, geojsons)
 tippecanoe -zg -o out.mbtiles --drop-densest-as-needed -l out --no-tile-compression $INPUT
 
-if [ -d $OUTPUT_DIR ]; then
-  mb-util --image_format=pbf ./out.mbtiles $OUTPUT_DIR
-  find $OUTPUT_DIR -name "*.pbf" -exec bash -c 'mv "$1" "${1%.pbf}".mvt' - '{}' \;
-  cp ./assets/index.html $OUTPUT_DIR
-  cat $OUTPUT_DIR/metadata.json | \
-  	jq ".tiles |= [\"https://${GITHUB_USERNAME}.github.io/${GITHUB_REPOSITORY}/{z}/{x}/{y}.mvt\"]" > \
-	tiles.json
-fi
+
+mb-util --image_format=pbf ./out.mbtiles $OUTPUT_DIR
+find $OUTPUT_DIR -name "*.pbf" -exec bash -c 'mv "$1" "${1%.pbf}".mvt' - '{}' \;
+cp ./assets/index.html $OUTPUT_DIR
+
+cat $OUTPUT_DIR/metadata.json | \
+  jq ".tiles |= [\"https://${GITHUB_USERNAME}.github.io/${GITHUB_REPOSITORY}/{z}/{x}/{y}.mvt\"]" > \
+  tiles.json
