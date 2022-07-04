8ba6a38f75b7a3550a40870048337112c0f474c8
kamataryo <kamataryo@users.noreply.github.com>
2022-07-04 11:45:48 +0900

    Fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,21 +2,16 @@
 
 set -eu
 
-GITHUB_USERNAME=$1
-INPUT=$2
-OUTPUT_DIR=$3
+INPUT=$1
+OUTPUT_DIR=$2
+BASE_URL="https://kamataryo.github.io/vector-tile-action"
 
-# $1: output.mbtiles
-# $1: input files (shape, geojsons)
 tippecanoe -zg -o out.mbtiles --drop-densest-as-needed -l out --no-tile-compression $INPUT
-
 
 mb-util --image_format=pbf ./out.mbtiles $OUTPUT_DIR
 find $OUTPUT_DIR -name "*.pbf" -exec bash -c 'mv "$1" "${1%.pbf}".mvt' - '{}' \;
 cp ./assets/index.html $OUTPUT_DIR
 
-echo $1
-
-# cat $OUTPUT_DIR/metadata.json | \
-#   jq ".tiles |= [\"https://${GITHUB_USERNAME}.github.io/${GITHUB_REPOSITORY}/{z}/{x}/{y}.mvt\"]" > \
-#   tiles.json
+cat $OUTPUT_DIR/metadata.json | \
+  jq ".tiles |= [\"${BASE_URL}/{z}/{x}/{y}.mvt\"]" > \
+  tiles.json
