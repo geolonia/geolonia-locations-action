d823caacc5f5d63d8a5cb7d7bfffac3e893b80bc
kamataryo <kamataryo@users.noreply.github.com>
2022-07-04 11:39:55 +0900

    Test commit

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,9 +3,8 @@
 set -eu
 
 GITHUB_USERNAME=$1
-GITHUB_REPOSITORY=$2
-INPUT=$3
-OUTPUT_DIR=$4
+INPUT=$2
+OUTPUT_DIR=$3
 
 # $1: output.mbtiles
 # $1: input files (shape, geojsons)
@@ -16,6 +15,8 @@ mb-util --image_format=pbf ./out.mbtiles $OUTPUT_DIR
 find $OUTPUT_DIR -name "*.pbf" -exec bash -c 'mv "$1" "${1%.pbf}".mvt' - '{}' \;
 cp ./assets/index.html $OUTPUT_DIR
 
-cat $OUTPUT_DIR/metadata.json | \
-  jq ".tiles |= [\"https://${GITHUB_USERNAME}.github.io/${GITHUB_REPOSITORY}/{z}/{x}/{y}.mvt\"]" > \
-  tiles.json
+echo $1
+
+# cat $OUTPUT_DIR/metadata.json | \
+#   jq ".tiles |= [\"https://${GITHUB_USERNAME}.github.io/${GITHUB_REPOSITORY}/{z}/{x}/{y}.mvt\"]" > \
+#   tiles.json
