4268981165cdda822dca3d866c0e9ff68edc6a0e
naoki <n.globe.us@gmail.com>
2022-10-12 14:43:28 +0900

    add tiles.json

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,8 +4,11 @@ set -e
 FILE=$1
 GEOLONIA_ACCESS_TOKEN=$2
 OUT_DIR=$3
+echo $4
 
 TILES_OUT_DIR=$OUT_DIR/tiles
+METADATA_JSON_DIR=$TILES_OUT_DIR/metadata.json
+echo $METADATA_JSON_DIR
 LAYER_NAME=data
 
 mkdir -p $TILES_OUT_DIR
@@ -22,4 +25,26 @@ else
     $FILE
 
   find $TILES_OUT_DIR -name "*.pbf" -exec sh -c 'mv "$1" "${1%.pbf}".mvt' - '{}' \;
+
+  if [ -f $METADATA_JSON_DIR ]
+  then
+  
+    cat $METADATA_JSON_DIR | jq '{
+      name: .name,
+      version: .version,
+      description: .description,
+      type: .type,
+      format: .format,
+      attribution: .attribution,
+      minzoom: .minzoom,
+      maxzoom: .maxzoom,
+      center: .center,
+      bounds: .bounds,
+      "tiles": [
+        "https://YOUR-GITHUB-USER.github.io/vector-tiles-api/tiles/{z}/{x}/{y}.mvt"
+      ]
+    }' > $TILES_OUT_DIR/tiles.json
+
+  fi
+
 fi
