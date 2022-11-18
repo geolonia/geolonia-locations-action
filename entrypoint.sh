76dc172e0962be8e7aa688f654f2298cfdaa7513
naoki <n.globe.us@gmail.com>
2022-11-18 09:47:51 +0900

    add default tipecanoe option for spactial-id-sdk to get all data at z0

--- entrypoint.sh
+++ entrypoint.sh
@@ -46,6 +46,9 @@ else
       --output-to-directory $TILES_OUT_DIR \
       --layer $LAYER_NAME \
       --no-tile-compression \
+      --drop-rate=1 \
+      --no-feature-limit \
+      --no-tile-size-limit \
       $FILE
 
   else
@@ -56,6 +59,9 @@ else
       --layer $LAYER_NAME \
       --drop-densest-as-needed \
       --no-tile-compression \
+      --drop-rate=1 \
+      --no-feature-limit \
+      --no-tile-size-limit \
       $FILE
   fi
 
